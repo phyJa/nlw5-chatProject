f679b8b54b19cfd9c86c8126a5aab48f949ba521
Elias Bueno Fonseca <eli.bueno9@gmail.com>
2021-04-21 13:57:02 -0300

    Add typeorm, reflect-metadata and sqlite3

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,18 @@ import express from "express";
 
 const app = express();
 
+app.get(
+    "/",
+    (request, response) => {
+        return response.json({message:"Hello!!!"});
+    }
+);
+
+app.post(
+    "/users",
+    (request, response) => {
+        return response.json({ message: "User registered successfully! "});
+    }
+)
+
 app.listen(3333, () => { console.log("Server is running on port 3333")});
